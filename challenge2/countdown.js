c9a5a68660119361839a1019992ed3ac8bd013b8
Michelle <michelle@tsociety.io>
2016-11-26 14:19:28 +0100

    countdown fully functional

---
+++ challenge2/countdown.js
@@ -0,0 +1,41 @@
+var deadline = 'December 2 2016';
+function getTimeRemaining(endtime){
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+ return {
+  'total': t,
+  'days': days,
+  'hours': hours,
+  'minutes': minutes,
+  'seconds': seconds
+};
+}
+getTimeRemaining(deadline).minutes;
+
+function initializeClock(id, endtime){
+  var clock = document.getElementById(id);
+  var daysSpan = clock.querySelector('.days');
+  var hoursSpan = clock.querySelector('.hours');
+  var minutesSpan = clock.querySelector('.minutes');
+  var secondsSpan = clock.querySelector('.seconds');
+
+function updateClock(){
+  var t = getTimeRemaining(endtime);
+
+  clock.innerHTML = 'days: ' + t.days + '<br>' +
+                    'hours: '+ t.hours + '<br>' +
+                    'minutes: ' + t.minutes + '<br>' +
+                    'seconds: ' + t.seconds;
+seconds  if(t.total<=0){
+    clearInterval(timeinterval);
+  }
+}
+
+updateClock(); // run function once at first to avoid delay
+var timeinterval = setInterval(updateClock,1000);
+}
+
+initializeClock('clockdiv', deadline);
